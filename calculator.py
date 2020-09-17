eb0cdf23e8820a965f9fc400e82e9d21769eb363
Enid-Sky <enid.sky@protonmail.com>
2020-09-17 12:45:33 -0700

    create basic functions

---
+++ calculator.py
@@ -0,0 +1,36 @@
+###################################
+#_SIMPLE PYTHON CALCULATOR PROGRAM_#
+###################################
+
+#################
+#Basic Functions#
+#################
+
+# ADDITION
+
+def add(x, y):
+    return x + y
+
+# SUBTRACTION
+
+
+def sub(x, y):
+    return x - y
+
+# MULTIPLICATION
+
+
+def mult(x, y):
+    return x * y
+
+# DIVISION
+
+
+def div(x, y):
+    return x / y
+
+# EXPONENT
+
+
+def expo(x, y):
+    return x ** y
